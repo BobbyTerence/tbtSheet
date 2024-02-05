629ffca2bbb58096b9d812f65e8a3e9debf0311a
Will Flowers <willtf2000@gmail.com>
2024-02-05 16:00:54 -0600

    Set Origin for the window

--- window.py
+++ window.py
@@ -8,7 +8,20 @@ root = Tk();
 root.title("Character Sheet")
 
 # Defualt Window size
-root.geometry('1280x720')
+w = 1280;
+h = 720;
+
+# Add creation location for the window
+#grab window size
+scWidth = root.winfo_screenmmwidth;
+scLength = root.winfo_screenheight;
+
+# Set position of window
+xOrigin = (scWidth/2) - (w/2);
+yOrigin = (scLength/2) - (h/2);
+
+# Set size and position of window
+root.geometry('%dx%d+%d+%d' % (w, h, xOrigin, yOrigin))
 
 # start the window
 root.mainloop();
